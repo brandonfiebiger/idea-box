36e8e6f39c739066799007513dbdf06ac6f865f6
Tim Fischer <emailtimfischer@gmail.com>
2018-05-19 18:38:26 -0600

    Added image assets folder

--- script.js
+++ script.js
@@ -3,14 +3,9 @@
 $('.save-button').on('click', function() {
   var title = $('.user-title').val();
   var idea = $('.user-idea').val();
-
-
-
-
-  $('.user-title-output').text(title);
-  $('.user-idea-output').text(idea);
-
-  $('.idea-display').prepend('<li><h1 class="user-title-output"></h1><p class="user-idea-output"></p></li>');
+   $('.user-title-output').text(title);
+   $('.user-idea-output').text(idea);
+   $('.idea-display').prepend('<li><h1 class="user-title-output"></h1><p class="user-idea-output"></p><img class="up-vote" src="images/upvote.svg"><img class="down-vote" src="images/downvote.svg"><hr></li>');
 
 });
 
