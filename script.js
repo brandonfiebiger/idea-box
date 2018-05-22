004998778d71693076efb43b2f7fbec0b538b199
Brandon Fiebiger <brandonfiebiger@gmail.com>
2018-05-22 17:56:14 -0600

    swill quality code

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 var title = $('.user-title').val();
 var body = $('.user-idea').val();
 var quality = 'quality: swill';
+var upVote = $('.up-vote');
 
 
 
@@ -18,30 +19,31 @@ $('.save-button').on('click', function() {
     <li>
       <h1 class="user-title-output"></h1>
       <p class="user-idea-output"></p>
-      <img class="up-vote" src="images/upvote.svg">
+      <img class="up-vote" src="images/upvote.svg" type="button">
       <img class="down-vote" src="images/downvote.svg">
       <p class="user-quality"></p>
       <hr>
     </li>
     `);
 
+   $('.up-vote').on('click', function(event) {
+      qualityUp();
+      console.log(event.target);
 });
+   $('.down-vote').on('click', function() {
+      qualityDown();
+   })
 
 
-
-function Useridea(id,title,body,quality){
-  this.id = id;
-  this.title= title;
-  this.body = body;
-  this.quality = quality;
-}
-
+});
 
 function addNewIdea(){
   this.id = $.now();
   this.title = $('.user-title').val();
   this.body = $('.user-idea').val();
-  var ideaToStore = { id: this.id, title: this.title, body: this.body, quality: "swill"};
+  this.quality = $('.user-quality').val();
+
+  var ideaToStore = { id: this.id, title: this.title, body: this.body, quality: this.quality};
   var stringifiedIdea = JSON.stringify(ideaToStore);
   localStorage.setItem(this.id, stringifiedIdea); 
   var retreivedIdea = localStorage.getItem(this.id);
@@ -69,11 +71,38 @@ for(var i = 0; i < localStorage.length; i++){
       </p>
       <img class="up-vote" src="images/upvote.svg">
       <img class="down-vote" src="images/downvote.svg">
-        
+      <p class="user-quality"></p>
       <hr>
     </li>
   `);
-
-
   }
 }
+
+function qualityDown() {
+  if (this.quality = 'swill') {
+    $('.user-quality').text('swill');
+  } else if (this.quality = 'plausible') {
+    $('.user-quality').text('swill');
+  } else if (this.quality = 'Genius') {
+    $('.user-quality').text('plausible');
+  }
+}
+
+function qualityUp() {
+  if (this.quality = 'swill') {
+    $('.user-quality').text('plausible');
+  } else if (this.quality = 'plausible') {
+    $('.user-quality').text('Genius');
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
