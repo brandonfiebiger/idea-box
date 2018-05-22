e8ccd0b22d8107d892444bb0672be5823c4612bf
Tim Fischer <emailtimfischer@gmail.com>
2018-05-22 15:46:47 -0600

    Removed used code

--- script.js
+++ script.js
@@ -6,14 +6,21 @@ var body = $('.user-idea').val();
 
 
 $('.save-button').on('click', function() {
-  var title = $('.user-title').val();
-  var body = $('.user-idea').val();
    $('.user-title-output').text(title);
    $('.user-idea-output').text(body);
    addNewIdea();
 
 
-   $('.idea-display').prepend('<li><h1 class="user-title-output"></h1><p class="user-idea-output"></p><img class="up-vote" src="images/upvote.svg"><img class="down-vote" src="images/downvote.svg"><hr></li>');
+
+   $('.idea-display').prepend(`
+    <li>
+      <h1 class="user-title-output"></h1>
+      <p class="user-idea-output"></p>
+      <img class="up-vote" src="images/upvote.svg">
+      <img class="down-vote" src="images/downvote.svg">
+      <hr>
+    </li>
+    `);
 
 });
 
@@ -21,7 +28,7 @@ $('.save-button').on('click', function() {
 
 function Useridea(id,title,body,quality){
   this.id = id;
-  this.title = title;
+  this.title= title;
   this.body = body;
   this.quality = quality;
 }
@@ -42,8 +49,6 @@ function addNewIdea(){
 
 
 function displayIdea(){
-  // var getIdea = localStorage.getItem('');
-
 for(var i = 0; i < localStorage.length; i++){
   var lsKey = Object.keys(localStorage)[i];
   var ideaOutput = JSON.parse(localStorage[lsKey]);
@@ -61,6 +66,7 @@ for(var i = 0; i < localStorage.length; i++){
       </p>
       <img class="up-vote" src="images/upvote.svg">
       <img class="down-vote" src="images/downvote.svg">
+        
       <hr>
     </li>
   `);
